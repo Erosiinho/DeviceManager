60566cac20c435f51348f7fcf668bc5eda602014
erosinho <erosalberola1@gmail.com>
2025-08-04 15:52:08 +0200

    Added .gitignore and updated routes methods

--- internal/routes/device.go
+++ internal/routes/device.go
@@ -8,7 +8,7 @@ import (
 func Register(apiGrp *echo.Group, controllerCtx *controllers.ControllerCtx) {
 	deviceGroup := apiGrp.Group("/devices")
 	deviceGroup.GET("", controllerCtx.GetAllDevices)
-	deviceGroup.POST("/:id/start", controllerCtx.StartDevice)
-	deviceGroup.POST("/:id/stop", controllerCtx.StopDevice)
+	deviceGroup.GET("/:id/start", controllerCtx.StartDevice)
+	deviceGroup.GET("/:id/stop", controllerCtx.StopDevice)
 	deviceGroup.GET("/:id/ping", controllerCtx.PingDevice)
 }
